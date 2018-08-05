4966ab1a4e3faf26f183fbf2263114fb3213c0b5
Adrian Nidzgorski <nidzgorskiadrian@gmail.com>
2018-08-05 20:50:01 +0200

    Add weather app to projects

--- projects.php
+++ projects.php
@@ -18,6 +18,17 @@
                 </a>
             </div>
 
+            <div class="col-xs-6 col-sm-4 boxProject text-center">
+                <a target="_blank" href="https://nidzgorski.pl/weatherapp">
+                    <div class="gallery">
+                        <img src="resources/images/projects/weatherapp.png">
+                        <div class="overlay">
+                            <div class="text">Aplikacja pogodowa</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
         </div>
     </div>
 
