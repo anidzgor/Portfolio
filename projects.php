efb11b8dadd27f8f7f08d5da114b946191679f7d
Adrian Nidzgorski <nidzgorskiadrian@gmail.com>
2018-08-05 18:40:48 +0200

    Remove site blog love2liveblog.pl

--- projects.php
+++ projects.php
@@ -18,17 +18,6 @@
                 </a>
             </div>
 
-            <div class="col-xs-6 col-sm-4 boxProject text-center">
-                <a target="_blank" href="https://love2liveblog.pl/">
-                    <div class="gallery">
-                        <img src="resources/images/projects/blog.png">
-                        <div class="overlay">
-                            <div class="text">Blog podróżniczy - Love2Live</div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
         </div>
     </div>
 
